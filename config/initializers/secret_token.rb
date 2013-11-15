cb6b6e4f4ae07f3e9d832e86fd34b20bc98124ef
Harley Inbody <hinbody@gmail.com>
2013-11-14 19:34:19 -0500

    initial commit plus setup Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BaconSplitter::Application.config.secret_key_base = '3cd554020103f088fc5c9da02160ba0968bd0b3b4d32953a32c4bfcbc90efe4652552949d05e931d70ce2bd18aa7d4adf761e5c1ccda29432a644b8a8d416449'
