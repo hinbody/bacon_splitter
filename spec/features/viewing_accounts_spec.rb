b318c1298f83cd6d56b602d98cb799957570bfaa
Harley Inbody <hinbody@gmail.com>
2013-12-30 22:01:17 -0500

    add spec to test for displaying average monthly bill on account#show

--- spec/features/viewing_accounts_spec.rb
+++ spec/features/viewing_accounts_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
 feature "Viewing accounts" do
-  scenario "Listing all accounts" do
-    account = FactoryGirl.create(:account, name: "Cable Co")
+    let!(:account) { FactoryGirl.create(:account, name: "Cable Co") }
+    let!(:bill) { FactoryGirl.create(:bill, account: account,
+                                    current_due: 20) }
+    let!(:bill1) { FactoryGirl.create(:bill, account: account,
+                                    current_due: 40) }
+
+    before do
     visit accounts_path
     click_link 'Cable Co'
+    end
+
+  scenario "Listing all accounts" do
     expect(page.current_url).to eql(account_url(account))
+  end
+
+  scenario "account#show should show the average monthly bill" do
+    expect(page).to have_content("Your average bill is 30.00")
   end
 end
