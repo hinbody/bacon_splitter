e171f420b618d6122f702f271a5d799489a473b7
Harley Inbody <hinbody@gmail.com>
2014-01-03 21:29:57 -0500

    Create User model with password/confirmation
    Generate a user model. Write specs to verify that the User: has a password, has
    a password_confirmation, and they match

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130161631) do
+ActiveRecord::Schema.define(version: 20140104015934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,5 +38,13 @@ ActiveRecord::Schema.define(version: 20131130161631) do
   end
 
   add_index "bills", ["account_id"], name: "index_bills_on_account_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
