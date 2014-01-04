ab5ccd752ad6fb96eb03216a58807fcd45914d07
Harley Inbody <hinbody@gmail.com>
2014-01-03 21:55:18 -0500

    Implement sign up feature
    A user can now click a sign up button and sign up for an account. The user is then
    redirected to accounts#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ BaconSplitter::Application.routes.draw do
   resources :accounts do
     resources :bills
   end
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
