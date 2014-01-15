46e3bfd1ae30b17b083a23105f3e6252951f0ab7
Harley Inbody <hinbody@gmail.com>
2014-01-15 18:53:30 -0500

    Write specs to test that a user can sign in and implement code to make specs pass

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ BaconSplitter::Application.routes.draw do
     resources :bills
   end
   resources :users
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
